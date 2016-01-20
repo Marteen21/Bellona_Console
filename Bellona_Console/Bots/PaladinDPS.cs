bddb38d3d937d14e75265c84e2fc8b22c2add985
Tormasan <jackcrever@gmail.com>
2016-01-20 01:23:10 +0100

    pale

--- Bellona_Console/Bots/PaladinDPS.cs
+++ Bellona_Console/Bots/PaladinDPS.cs
@@ -5,13 +5,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Bellona_Console.Controller;
 
 namespace Bellona_Console.Bots {
     class PaladinDPS : DPSBot {
+
+        private Spell might = new Spell(79102,ConstController.WindowsVirtualKey.K_6);
+        private Spell plea = new Spell(0, ConstController.WindowsVirtualKey.K_7);
+        private Spell exorcism = new Spell(59578, ConstController.WindowsVirtualKey.VK_F10); //art of war
+        private Spell inquisition = new Spell(84963, ConstController.WindowsVirtualKey.K_8);
+        private Spell truth = new Spell(31801, ConstController.WindowsVirtualKey.K_9);
+        private Spell judgement = new Spell(89906, ConstController.WindowsVirtualKey.VK_F12);
+        private Spell templar = new Spell(90174, ConstController.WindowsVirtualKey.VK_F11);
+        private Spell crusader = new Spell(0, ConstController.WindowsVirtualKey.VK_F9);
+        private Spell hammer = new Spell(31884,ConstController.WindowsVirtualKey.VK_F8);//avenging wrath buff alat ingyen lehet tolni
+
         public PaladinDPS(BlackMagic wowProcess, WoWGlobal globalinfo, uint tt) : base(wowProcess, globalinfo, tt) {
         }
+
         public override void Rota() {
-            throw new NotFiniteNumberException();
+            if (!this.Player.Unit.HasBuff(might.ID)) {
+                might.SendCast();
+            }
+            if (!this.Player.Unit.HasBuff(truth.ID)) {
+                truth.SendCast();
+            }
+            if(this.Player.Unit.GetManaPercent()<50  && this.Player.Unit.GetHealthPercent() > 70) {
+                plea.SendCast();
+            }
+            if (this.Player.Unit.HasBuff(exorcism.ID)) {
+                exorcism.SendCast();
+            }
+            if (!this.Player.Unit.HasBuff(inquisition.ID) && (this.Player.Unit.HolyPower == 3 || this.Player.Unit.HasBuff(templar.ID) )) {
+                inquisition.SendCast();
+            }
+            judgement.SendCast();
+            crusader.SendCast();
+            if(this.Player.Unit.HolyPower == 3 || this.Player.Unit.HasBuff(templar.ID)) {
+                templar.SendCast();
+            }
+            if (this.Target.Unit.GetHealthPercent()>20 || this.Player.Unit.HasBuff(hammer.ID)) {
+                hammer.SendCast();
+            }
         }
     }
 }
